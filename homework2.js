a057250278925d4ffd525b9a47db5c415b9b8bf6
quanln94 <62424279+quanln94@users.noreply.github.com>
2020-03-26 23:02:00 +0700

    Delete homework2.js

--- homework2.js
+++
@@ -1,5 +0,0 @@
-// tính diện tích hình tròn
-const a = Number(prompt(" Nhập bán kính hình tròn( đơn vị cm ): "));
-const s = a * 3.14159;
-const b = "Diện tích hình tròn là: " + s + " cm2";
-console.log(b);
